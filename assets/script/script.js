7c780f85ed2aef4acc89185481a43f0d64c1f8c1
SimonCzEdu <simonczedu@gmail.com>
2024-06-03 17:37:40 +0100

    Script: highlights functionality version 0.5

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,40 @@
-/**
- * 
- */
-function highlight_on_mouseover() {
-    document.getElementById('highlightOneDiv')
-}
+document.getElementById('highlightsContainer').addEventListener('mouseover', (e) => {
+    const { clientX: x, clientY: y } = e
+    const elementMouseIsOver = document.elementFromPoint(x, y);
+    if (elementMouseIsOver.id == `highlightOneDiv`) {
+        document.getElementById(`highlightOneImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightTwoDiv`) {
+        document.getElementById(`highlightTwoImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightThreeDiv`) {
+        document.getElementById(`highlightThreeImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightFourDiv`) {
+        document.getElementById(`highlightFourImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightFiveDiv`) {
+        document.getElementById(`highlightFiveImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightSixDiv`) {
+        document.getElementById(`highlightSixImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightSevenDiv`) {
+        document.getElementById(`highlightSevenImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightEightDiv`) {
+        document.getElementById(`highlightEightImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightNineDiv`) {
+        document.getElementById(`highlightNineImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightTenDiv`) {
+        document.getElementById(`highlightTenImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightElevenDiv`) {
+        document.getElementById(`highlightElevenImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightTwelveDiv`) {
+        document.getElementById(`highlightTwelveImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightThirteenDiv`) {
+        document.getElementById(`highlightThirteenImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightFourteenDiv`) {
+        document.getElementById(`highlightFourteenImg`).style.visibility = `visible`;
+    } else if (elementMouseIsOver.id == `highlightFifteenDiv`) {
+        document.getElementById(`highlightFifteenImg`).style.visibility = `visible`;
+    } else {
+        const highlightsDiv = document.getElementsByClassName('allHighlightsImg');
+        for (let i = 0; i < highlightsDiv.length; i++) {
+            highlightsDiv[i].style.visibility = `hidden`;
+        }
+    }
+})
