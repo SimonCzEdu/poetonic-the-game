c3e1be7e680ec62fa604cb93cbc98c833698c6cd
SimonCzEdu <simonczedu@gmail.com>
2024-06-06 23:14:55 +0100

    Style: add and adjust new images for highlights

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,40 +1,27 @@
-document.getElementById('highlightsContainer').addEventListener('mouseover', (e) => {
-    const { clientX: x, clientY: y } = e
-    const elementMouseIsOver = document.elementFromPoint(x, y);
-    if (elementMouseIsOver.id == `highlightOneDiv`) {
-        document.getElementById(`highlightOneImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightTwoDiv`) {
-        document.getElementById(`highlightTwoImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightThreeDiv`) {
-        document.getElementById(`highlightThreeImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightFourDiv`) {
-        document.getElementById(`highlightFourImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightFiveDiv`) {
-        document.getElementById(`highlightFiveImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightSixDiv`) {
-        document.getElementById(`highlightSixImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightSevenDiv`) {
-        document.getElementById(`highlightSevenImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightEightDiv`) {
-        document.getElementById(`highlightEightImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightNineDiv`) {
-        document.getElementById(`highlightNineImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightTenDiv`) {
-        document.getElementById(`highlightTenImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightElevenDiv`) {
-        document.getElementById(`highlightElevenImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightTwelveDiv`) {
-        document.getElementById(`highlightTwelveImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightThirteenDiv`) {
-        document.getElementById(`highlightThirteenImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightFourteenDiv`) {
-        document.getElementById(`highlightFourteenImg`).style.visibility = `visible`;
-    } else if (elementMouseIsOver.id == `highlightFifteenDiv`) {
-        document.getElementById(`highlightFifteenImg`).style.visibility = `visible`;
-    } else {
-        const highlightsDiv = document.getElementsByClassName('allHighlightsImg');
-        for (let i = 0; i < highlightsDiv.length; i++) {
-            highlightsDiv[i].style.visibility = `hidden`;
-        }
-    }
-})
+function idUnderPointer(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const elementMouseIsOver = document.elementFromPoint(x, y).id;
+    highlightOn(elementMouseIsOver)
+}
+
+function highlightOn(id) {
+    document.getElementById(id).style.opacity = '1';
+    console.log(id)
+}
+
+document.getElementById(`highlightOne`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightTwo`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightThree`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightFour`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightFive`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightSix`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightSeven`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightEight`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightNine`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightTen`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightEleven`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightTwelve`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightThirteen`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightFourteen`).addEventListener(`mouseover`, idUnderPointer);
+document.getElementById(`highlightFifteen`).addEventListener(`mouseover`, idUnderPointer);
